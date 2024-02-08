5b6136dabc95f8074dae429927ead076254d704e
la5er <larinserg@gmail.com>
2024-02-08 11:02:13 +0700

    aiogram last version update

--- main.py
+++ main.py
@@ -1,48 +1,56 @@
+import asyncio
 import logging
+import sys
 import os
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+from aiogram import Bot, Dispatcher, Router, types, F
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils.markdown import hbold
 
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO)
-
 TOKEN = os.getenv("BOT_TOKEN")
-if not TOKEN:
-    raise Exception("Bot token not found in environment variable. Make sure to set BOT_TOKEN in your .env file.")
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-my_profile_button = KeyboardButton("\U0001F464 Мой профиль")
-my_events_button = KeyboardButton("\U0001F5D3 Мои мероприятия")
-help_button = KeyboardButton("\U00002754 Помощь")
 
-main_menu_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(
-    my_profile_button
-).add(
-    my_events_button
-).add(
-    help_button
-)
+@dp.message(CommandStart())
+async def cmd_start(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="\U0001F464 Мой профиль"),
+            types.KeyboardButton(text="\U0001F5D3 Мои мероприятия"),
+            types.KeyboardButton(text="\U00002754 Помощь")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите пункт меню"
+    )
+    await message.answer("Вас приветсвует Event бот СГУ", reply_markup=keyboard)
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.answer("Привет! Это твой бот.", reply_markup=main_menu_keyboard)
+@dp.message(F.text == "\U0001F464 Мой профиль")
+async def with_puree(message: types.Message):
+    await message.answer("Функция 'Профиль' еще не реализована. Подождите немного!")
 
-@dp.message_handler(lambda message: message.text == my_profile_button.text)
-async def my_profile(message: types.Message):
-    await message.answer("Функция 'Мой профиль' еще не реализована. Подождите немного!")
-
-@dp.message_handler(lambda message: message.text == my_events_button.text)
-async def my_events(message: types.Message):
+@dp.message(F.text == "\U0001F5D3 Мои мероприятия")
+async def without_puree(message: types.Message):
     await message.answer("Функция 'Мои мероприятия' еще не реализована. Подождите немного!")
 
-@dp.message_handler(lambda message: message.text == help_button.text)
-async def help_command(message: types.Message):
+@dp.message(F.text == "\U00002754 Помощь")
+async def without_puree(message: types.Message):
     await message.answer("Функция 'Помощь' еще не реализована. Подождите немного!")
 
-if __name__ == '__main__':
-    from aiogram import executor
-    executor.start_polling(dp, skip_updates=True)
+
+async def main() -> None:
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
